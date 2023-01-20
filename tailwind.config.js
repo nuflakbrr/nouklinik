73dcfb46e3aa3ef1c09ea12965a1a604b39db6b2
Naufal Akbar <kbrnugroho@gmail.com>
2023-01-20 13:30:48 +0700

    upd: update tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode:'class',
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#01a3a4',
+        primary2: '#017d7e',
+      },
+      fontFamily: {
+        primary: ['DM Sans', 'sans-serif'],
+        secondary: ['Open Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
