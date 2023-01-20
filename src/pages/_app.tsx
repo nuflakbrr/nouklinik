0ef28a6128cf4fc6b8f67cabbc9c8ff61230bd0b
Naufal Akbar <kbrnugroho@gmail.com>
2023-01-20 13:32:33 +0700

    upd: implement darkmode, layout, base meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from 'next-themes';
+
+import Navbar from '../components/Common/Navbar/Navbar';
+import ScrollToTop from '../components/ScrollToTop';
+import Footer from '../components/Common/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport' content='width=device-width, initial-scale=1" />
+        <meta name="theme-color' content='#000000" />
+        <link
+          rel="shortcut icon"
+          href="/assets/favicon.png"
+          type="image/x-icon"
+        />
+      </Head>
+
+      <Navbar />
+
+      <ScrollToTop />
+
+      <Component {...pageProps} />
+
+      <Footer />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
