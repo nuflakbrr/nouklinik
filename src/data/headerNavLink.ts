11f0b95caa2e11d82130677f4ecdfcf4da897932
Naufal Akbar <kbrnugroho@gmail.com>
2023-01-20 13:32:53 +0700

    feat: create headerNavLink data

---
+++ src/data/headerNavLink.ts
@@ -0,0 +1,7 @@
+export const headerNavLink = [
+  { title: 'Beranda', href: '/' },
+  { title: 'Tentang', href: '/about' },
+  { title: 'Fasilitas', href: '/service' },
+  { title: 'Dokter', href: '/doctors' },
+  { title: 'Kontak', href: '/contact' },
+];
