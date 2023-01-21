a648e951028b6c27c5910b96bf7799a28f9a6bda
Naufal Akbar <kbrnugroho@gmail.com>
2023-01-21 21:40:35 +0700

    update navlink

--- src/data/headerNavLink.ts
+++ src/data/headerNavLink.ts
@@ -2,7 +2,7 @@ export const headerNavLink = [
   { title: 'Beranda', href: '/' },
   { title: 'Tentang', href: '/about' },
   { title: 'Layanan', href: '/service' },
-  { title: 'Dokter', href: '/doctors' },
   { title: 'Fasilitas', href: '/facilities' },
+  { title: 'Dokter', href: '/doctors' },
   { title: 'Kontak', href: '/contact' },
 ];
