7b9b5f1edbfc14f0a9cfabe58750a544a988787b
Naufal Akbar <kbrnugroho@gmail.com>
2023-01-25 21:41:39 +0700

    feat: create doctors page

---
+++ src/data/doctors.ts
@@ -0,0 +1,191 @@
+export const doctorsData = [
+  {
+    name: 'John McCulling',
+    specialism: 'Penyakit Dalam',
+    img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
+    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    recentSchools: [
+      { schName: 'unpad', schYear: '1983' },
+      { schName: 'unpad', schYear: '1983' },
+    ],
+    regulerSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    BPJSSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    slug: 'jhon-mcculling',
+  },
+  {
+    name: 'John McCulling',
+    specialism: 'Penyakit Dalam',
+    img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
+    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    recentSchools: [
+      { schName: 'unpad', schYear: '1983' },
+      { schName: 'unpad', schYear: '1983' },
+    ],
+    regulerSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    BPJSSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    slug: 'jhon-mcculling',
+  },
+  {
+    name: 'John McCulling',
+    specialism: 'Penyakit Dalam',
+    img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
+    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    recentSchools: [
+      { schName: 'unpad', schYear: '1983' },
+      { schName: 'unpad', schYear: '1983' },
+    ],
+    regulerSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    BPJSSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    slug: 'jhon-mcculling',
+  },
+  {
+    name: 'John McCulling',
+    specialism: 'Penyakit Dalam',
+    img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
+    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    recentSchools: [
+      { schName: 'unpad', schYear: '1983' },
+      { schName: 'unpad', schYear: '1983' },
+    ],
+    regulerSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    BPJSSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    slug: 'jhon-mcculling',
+  },
+  {
+    name: 'John McCulling',
+    specialism: 'Penyakit Dalam',
+    img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
+    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    recentSchools: [
+      { schName: 'unpad', schYear: '1983' },
+      { schName: 'unpad', schYear: '1983' },
+    ],
+    regulerSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    BPJSSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    slug: 'jhon-mcculling',
+  },
+  {
+    name: 'John McCulling',
+    specialism: 'Penyakit Dalam',
+    img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
+    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    recentSchools: [
+      { schName: 'unpad', schYear: '1983' },
+      { schName: 'unpad', schYear: '1983' },
+    ],
+    regulerSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    BPJSSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    slug: 'jhon-mcculling',
+  },
+  {
+    name: 'John McCulling',
+    specialism: 'Penyakit Dalam',
+    img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
+    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    recentSchools: [
+      { schName: 'unpad', schYear: '1983' },
+      { schName: 'unpad', schYear: '1983' },
+    ],
+    regulerSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    BPJSSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    slug: 'jhon-mcculling',
+  },
+  {
+    name: 'John McCulling',
+    specialism: 'Penyakit Dalam',
+    img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
+    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    recentSchools: [
+      { schName: 'unpad', schYear: '1983' },
+      { schName: 'unpad', schYear: '1983' },
+    ],
+    regulerSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    BPJSSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    slug: 'jhon-mcculling',
+  },
+  {
+    name: 'John McCulling',
+    specialism: 'Penyakit Dalam',
+    img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
+    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    recentSchools: [
+      { schName: 'unpad', schYear: '1983' },
+      { schName: 'unpad', schYear: '1983' },
+    ],
+    regulerSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    BPJSSchedule: [
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+      { day: 'Senin', time: '08.00 - 12.00' },
+    ],
+    slug: 'jhon-mcculling',
+  },
+];
---
+++ src/pages/doctors/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import ContainerDoctors from '../../components/Containers/Doctors/Doctors';
+
+const Doctors: FC = () => {
+  return <ContainerDoctors />;
+};
+
+export default Doctors;
