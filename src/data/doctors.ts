52cdab2ce8dd6d3db09f34608d87b5a6bad87523
Naufal Akbar <kbrnugroho@gmail.com>
2023-01-27 11:36:04 +0700

    upd: for test nested routes

--- src/data/doctors.ts
+++ src/data/doctors.ts
@@ -1,9 +1,9 @@
 export const doctorsData = [
   {
-    name: 'John McCulling',
+    name: 'Naufal Akbar',
     specialism: 'Penyakit Dalam',
     img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
-    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    desc: 'Prof. DR. dr. Naufal Akbar, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
     recentSchools: [
       { schName: 'unpad', schYear: '1983' },
       { schName: 'unpad', schYear: '1983' },
@@ -18,13 +18,13 @@ export const doctorsData = [
       { day: 'Senin', time: '08.00 - 12.00' },
       { day: 'Senin', time: '08.00 - 12.00' },
     ],
-    slug: 'jhon-mcculling',
+    slug: 'naufal-akbar',
   },
   {
-    name: 'John McCulling',
+    name: 'Akbar',
     specialism: 'Penyakit Dalam',
     img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
-    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    desc: 'Prof. DR. dr. Akbar, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
     recentSchools: [
       { schName: 'unpad', schYear: '1983' },
       { schName: 'unpad', schYear: '1983' },
@@ -39,13 +39,13 @@ export const doctorsData = [
       { day: 'Senin', time: '08.00 - 12.00' },
       { day: 'Senin', time: '08.00 - 12.00' },
     ],
-    slug: 'jhon-mcculling',
+    slug: 'akbar',
   },
   {
-    name: 'John McCulling',
+    name: 'Nopal',
     specialism: 'Penyakit Dalam',
     img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
-    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    desc: 'Prof. DR. dr. Nopal, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
     recentSchools: [
       { schName: 'unpad', schYear: '1983' },
       { schName: 'unpad', schYear: '1983' },
@@ -60,13 +60,13 @@ export const doctorsData = [
       { day: 'Senin', time: '08.00 - 12.00' },
       { day: 'Senin', time: '08.00 - 12.00' },
     ],
-    slug: 'jhon-mcculling',
+    slug: 'nopal',
   },
   {
-    name: 'John McCulling',
+    name: 'Nugroho',
     specialism: 'Penyakit Dalam',
     img: 'https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=500',
-    desc: 'Prof. DR. dr. Djoni Djunaedi, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
+    desc: 'Prof. DR. dr. Nugroho, Sp. PD-KPTI adalah sosok yang rendah hati dan murah senyum. Banyak orang akrab memanggil beliau Prof. Djoni. Sudah banyak pasien yang merasa puas dan senang setelah ditangani Prof. Djoni. Beliau merupakan lulusan Fakultas Kedokteran di Universitas Padjadjaran Bandung dengan Program Studi Ilmu Penyakit Dalam dan ditetapkan sebagai Dokter Spesialis I Penyakit Dalam (Sp.PD.) di Bandung pada tanggal 16 April 1983.',
     recentSchools: [
       { schName: 'unpad', schYear: '1983' },
       { schName: 'unpad', schYear: '1983' },
@@ -81,7 +81,7 @@ export const doctorsData = [
       { day: 'Senin', time: '08.00 - 12.00' },
       { day: 'Senin', time: '08.00 - 12.00' },
     ],
-    slug: 'jhon-mcculling',
+    slug: 'nugroho',
   },
   {
     name: 'John McCulling',
