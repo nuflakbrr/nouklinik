ef3a2ee278ad86e2a3862492af225f83d6a1d0b3
Naufal Akbar <kbrnugroho@gmail.com>
2023-01-29 15:50:38 +0700

    feat: disable scroll to top and activate live chat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 
 import Navbar from '../components/Common/Navbar/Navbar';
-import ScrollToTop from '../components/ScrollToTop';
 import Footer from '../components/Common/Footer';
+
+/**
+ * Un-comment ScrollToTop to active scroll to top button
+ * import ScrollToTop from '../components/ScrollToTop';
+ */
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -22,11 +27,33 @@ function MyApp({ Component, pageProps }: AppProps) {
 
       <Navbar />
 
-      <ScrollToTop />
+      {/* <ScrollToTop /> */}
 
       <Component {...pageProps} />
 
       <Footer />
+
+      <Script id="tawkto" strategy="lazyOnload">
+        {`
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          (function(){
+          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/63d625f0c2f1ac1e20302956/1gnu88dp1';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
+          })();
+        `}
+      </Script>
+
+      {/*
+        Un-comment below to active Tidio chat
+        <script
+          src="//code.tidio.co/qusxifbrlye534wenknkngvqp3k2mdw8.js"
+          async
+        />
+      */}
     </>
   );
 }
